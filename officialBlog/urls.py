e5dfe16fd76e5eadcb7acc2c32740b1959cd7fbd
sammy ocharo <obanyisammy@gmail.com>
2021-10-04 16:21:40 +0300

    created the crud api

--- officialBlog/urls.py
+++ officialBlog/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 
-urlpatterns = []
+from . import views
+
+urlpatterns = [
+    path('', views.apiOverView, name='apioverview'),
+    path('blog-list/', views.blogList, name='blog-list'),
+    path('blog-detail/<str:pk>/', views.blogDetail, name='blog-detail'),
+    path('blog-create/', views.createBlog, name='create-blog'),
+    path('blog-update/<str:pk>/', views.updateBlog, name='blog-update'),
+    path('blog-delete/<str:pk>/', views.deleteBlog, name='blog-delete'),
+]
--- officialBlog/views.py
+++ officialBlog/views.py
@@ -1,4 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import JsonResponse
+from rest_framework import serializers
 
+from rest_framework .decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import BlogSerializers
+from .models import Blogs
 # Create your views here.
 
+
+@api_view(['GET'])
+def apiOverView(request):
+    All_Blogs = Blogs.objects.all()
+    serializer = BlogSerializers(All_Blogs)
+    api_urls = {
+        'List': 'AllBlogs',
+        'Detail-view': '/blogPost-detail/<str:pk/>',
+        'update-view': 'blog-create/<str:pk/>',
+        'create-view': 'blog-create/',
+        'delete-view': 'blog-delete/',
+
+    }
+    
+    return Response(serializers)
+
+@api_view(['GET'])
+def blogList(request):
+    All_Blogs = Blogs.objects.all()
+    serializer = BlogSerializers(All_Blogs, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def blogDetail(request, pk):
+    All_Blogs = Blogs.objects.get(id=pk)
+    serializer = BlogSerializers(All_Blogs, many=False)
+    
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createBlog(request):
+    serializer = BlogSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateBlog(request, pk):
+    blog = Blogs.objects.get(id=pk)
+    serializer = BlogSerializers(instance=blog, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteBlog(request, pk):
+    blog = Blogs.objects.get(id=pk)
+    blog.delete()
+    
+    return Response('Blog deleted')
